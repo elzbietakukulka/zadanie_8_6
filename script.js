6ddef166b6290b2d48d02bd6bd9c32a97580270a
Elżbieta Kukułka <ellakaras@gmail.com>
2018-02-21 18:23:11 +0100

    Create script.js with a code

---
+++ script.js
@@ -0,0 +1,9 @@
+var a = 5;
+var b = 9;
+var value = (a * a) - (2 * a * b) - (b * b);
+
+console.log(value);
+
+if (value > 0) console.log('Wynik jest dodatni');
+else if (value == 0) console.log('Wynik jest równy 0');
+else console.log('Wynik jest ujemny');
